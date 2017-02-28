2c08f3655c67348499682a65cf857d5fcb1a09fe
root <soumya.parida@outlook.com>
2017-02-28 20:24:53 +0100

    adding

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,9 +13,41 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
 
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import (
+   password_reset,
+   password_reset_done,
+   password_reset_confirm,
+   password_reset_complete,
+    password_change,
+password_change_done
+)
+
+from signUp import views as core_views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', core_views.home, name='home'),
+url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        core_views.activate, name='activate'),
+    url('^', include('django.contrib.auth.urls')),
+    #url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+
+    # change password urls
+    url(r'^password-change/$', password_change, name='password_change'),
+    url(r'^password-change/done/$', password_change_done, name='password_change_done'),
+
+    # restore password urls
+    url(r'^password-reset/$',password_reset,{'template_name': 'password_reset_form.html'}, name='password_reset'),
+    url(r'^password-reset/done/$', password_reset_done,{'template_name': 'password_reset_done.html'}, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        password_reset_confirm,{'template_name': 'password_reset_confirm.html'},name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', password_reset_complete,{'template_name': 'password_reset_complete.html'},
+        name='password_reset_complete'),
 ]
--- mysite/signUp/views.py
+++ mysite/signUp/views.py
@@ -1,3 +1,100 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import render, redirect
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
 
-# Create your views here.
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.forms import SetPasswordForm
+from django.contrib.auth.forms import PasswordChangeForm
+
+#from mysite.core.forms import SignUpForm
+#from mysite.core.tokens import account_activation_token
+
+#from signUp import SignUpForm
+#from signUp.tokens import account_activation_token
+#from django.contrib.auth import update_session_auth_hash
+
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+#from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
+
+from signUp.forms import SignUpForm
+from signUp.tokens import account_activation_token
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            #user.refresh_from_db()  # load the profile instance created by the signal
+            #user.profile.myteam = form.get('myteam')
+            #print ("*******",user.profile.myteam)
+            current_site = get_current_site(request)
+            subject = 'Activate Your MySite Account'
+            message = render_to_string('account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+
+            return redirect('account_activation_sent')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def account_activation_sent(request):
+    return render(request, 'account_activation_sent.html')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
+        user.save()
+        login(request, user)
+        return redirect('home')
+    else:
+        return render(request, 'account_activation_invalid.html')
+
+def logout_view(request):
+    logout(request)
+
+def password_change(request,password_change_form=PasswordChangeForm):
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+    return render(request,'password_change_form.html')
+
+def password_change_done(request):
+    return render(request,'password_change_done.html')
+
+#def password_reset()
